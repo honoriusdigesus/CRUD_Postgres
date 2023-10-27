d4716c6ca58889915d7ad477ec39607e6cfe35c1
Honorius di Gesus <honoriusdigesus@gmail.com>
2023-10-27 11:58:53 -0500

    Cargando todos los datos de la tabla persona - OK

--- src/ConexionDB.java
+++ src/ConexionDB.java
@@ -1,5 +1,6 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class ConexionDB {
@@ -27,6 +28,20 @@ public class ConexionDB {
             System.out.println("Dates inserts");
         } catch (Exception e) {
             System.out.printf("Error inserting data: "+e.getMessage());
+        }
+    }
+
+    //Ver todos los datos de la tabla
+    public void selectDates(Connection connection, String nameTable){
+        try {
+            String query = String.format("SELECT * FROM %s;", nameTable);
+            statement = connection.createStatement();
+            ResultSet rs = statement.executeQuery(query);
+            while (rs.next()) {
+                System.out.println(rs.getString("di") + " " + rs.getString("name") + " " + rs.getString("lastname") + " " + rs.getString("age") + " " + rs.getString("email"));
+            }
+        } catch (Exception e) {
+            System.out.printf("Error selecting data: "+e.getMessage());
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,8 @@ public class Main {
     public static void main(String[] args) {
         ConexionDB db= new ConexionDB();
         Connection connection = db.getConnection("ejercicio_crud","postgres","postgres");
-        db.insertDates(connection,"persona",1104377381,"Honorius di Gesus","Jaramillo Ortega",34,"honoriusdigesus@gmail.com");
+        //db.insertDates(connection,"persona",1104381769,"Mary Cruz","Correa Paredes",28,"maryccorrea0117@gmail.com");
+        db.selectDates(connection,"persona");
 
     }
 }
