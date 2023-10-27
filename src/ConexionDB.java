c4a67d11b22925b43dbdf36cbc4a8ae7cbd073ee
Honorius di Gesus <honoriusdigesus@gmail.com>
2023-10-26 19:29:49 -0500

    Insertando datos en la tabla persona - OK

--- src/ConexionDB.java
+++ src/ConexionDB.java
@@ -1,9 +1,12 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class ConexionDB {
+    Connection connection = null;
+    Statement statement = null;
     public Connection getConnection(String dbName, String user, String password) {
-        Connection connection = null;
+
         try {
             Class.forName("org.postgresql.Driver");
             connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/" + dbName, user, password);
@@ -13,5 +16,17 @@ public class ConexionDB {
             System.out.printf("Error getting connection: "+e.getMessage());
         }
         return connection;
+    }
+
+    //Insertar datos en la tabla
+    public void insertDates(Connection connection, String nameTable, Integer di, String name, String lastName, Integer age, String email){
+        try {
+            String query = String.format("INSERT INTO %s(di, name, lastname, age, email) values ('%s','%s','%s','%s','%s');", nameTable, di, name, lastName, age, email);
+            statement = connection.createStatement();
+            statement.executeUpdate(query);
+            System.out.println("Dates inserts");
+        } catch (Exception e) {
+            System.out.printf("Error inserting data: "+e.getMessage());
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ import java.sql.Connection;
 public class Main {
     public static void main(String[] args) {
         ConexionDB db= new ConexionDB();
-        db.getConnection("ejercicio_crud","postgres","postgres");
+        Connection connection = db.getConnection("ejercicio_crud","postgres","postgres");
+        db.insertDates(connection,"persona",1104377381,"Honorius di Gesus","Jaramillo Ortega",34,"honoriusdigesus@gmail.com");
 
     }
 }
